bf053103fbe728a5a06a7798f194ac55a5d2a4ac
Camilo Canclini <camilocanclini@gmail.com>
2022-12-13 14:12:32 -0300

    ABM View Movies Feature Added

--- scripts/serverQuery.js
+++ scripts/serverQuery.js
@@ -1,31 +1,31 @@
 const containerPrint = document.getElementById('containerMoviesDB');
 (function serverRequest(){
-    const HTTPREQUEST = new XMLHttpRequest(); //Se crea objeto HTTPREQUEST
+    const HTTPREQUEST2 = new XMLHttpRequest(); //Se crea objeto HTTPREQUEST
     url = "../../php/get_movies.php"
-    HTTPREQUEST.open('GET',url, true); //Se configura la petición
+    HTTPREQUEST2.open('GET',url, true); //Se configura la petición
     /*Comienzan las operaciones AJAX*/
-    HTTPREQUEST.onreadystatechange = function(){
+    HTTPREQUEST2.onreadystatechange = function(){
         /*Se verifica que la petición fue exitosa*/
         if(this.readyState == 4 && this.status == 200){
             var respuesta = JSON.parse(this.responseText);//Se obtiene la respuesta de la API y se transforma array
             console.log(respuesta);
             let movies = respuesta;
             console.log('movies:', movies)
-            showResponse(movies, containerPrint);         
+            showResponse2(movies, containerPrint);         
         }
     }
-    HTTPREQUEST.send();
+    HTTPREQUEST2.send();
 }
 )();
-function showResponse(movies, containerPrint){
+function showResponse2(movies, containerPrint){
 	console.log(movies);
-	containerPrint.innerHTML = "";
 	/*Se recorre el ARRAY y se pintan los datos que contiene*/
 	for(let i of movies){
 		console.log('Pintando Objetos...');
 		console.log(i);
 
         let movieContainer = document.createElement("DIV");
+        movieContainer.setAttribute("CLASS", "format-table");
 
 		let id = document.createElement("DIV");
 		let titulo = document.createElement("DIV");
